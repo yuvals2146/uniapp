1821d6533140efd8886ea0d561405b52fdb621f7
yuvals2146 <yuvals2146@gmail.com>
2023-07-05 22:51:02 +0300

    Add whatsapp alert and block number to db

--- runner.js
+++ runner.js
@@ -1,22 +1,29 @@
-const { getPostionData,getPoolexchangeRate } = require("./getPostionData.js");
+const { getPostionData,getPoolexchangeRate,getCurrentBlockNumber } = require("./getPostionData.js");
 const {
   savePositionDataRedis,
   savePositionDataSQL,
 } = require("./savePositionData.js");
 const { queryTheGraph } = require("./queryTheGraph.js");
-
-
+const { notifiy, initNotifer } = require("./notifer.js");
+const {checkForAlerts} = require("./alerts.js");
 async function main() {
+  initNotifer();
   const poolId = 689765;
   const etherUsdExchangeRate = await getPoolexchangeRate(process.env.ETHER_USDC_POOL_ADDRESS);
   const ArbitUsdExchangeRate = await getPoolexchangeRate(process.env.ARB_USDC_POOL_ADDRESS);
-  
   const postionDataFromContract = await getPostionData(poolId);
-
-  await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
-
+  const currentBlockNumber = await getCurrentBlockNumber();
+  await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
   //const postionDataFromTheGraph = await queryTheGraph(poolId);
-
+  //await checkForAlerts(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
+  //await notifiy();
 }
 
-main();
+(function loop() {
+  setTimeout(() => {
+    main();
+
+    loop();
+  }, process.env.INTERVAL);
+})();
+
