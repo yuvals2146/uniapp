5fbfb2a66f5a67b50ff034308a400def941819c1
Dany Azriel <danyazriel@Danys-MacBook-Pro.local>
2023-07-13 18:00:20 +0300

    test commit

--- runner.js
+++ runner.js
@@ -1,19 +1,30 @@
-const { getPostionData,getPoolexchangeRate,getCurrentBlockNumber } = require("./getPostionData.js");
+const {
+  getPostionData,
+  getPoolexchangeRate,
+  getCurrentBlockNumber,
+} = require("./getPostionData.js");
 const {
   savePositionDataRedis,
   savePositionDataSQL,
 } = require("./savePositionData.js");
 const { queryTheGraph } = require("./queryTheGraph.js");
-const { notifiy, initNotifer } = require("./notifer.js");
-const {checkForAlerts} = require("./alerts.js");
+// const { notifiy, initNotifer } = require("./notifer.js");
+const { checkForAlerts } = require("./alerts.js");
+const { analyzeDataPoint } = require("./engine/core_module.js");
+
 async function main() {
-  initNotifer();
+  // initNotifer();
   const poolId = 689765;
-  const etherUsdExchangeRate = await getPoolexchangeRate(process.env.ETHER_USDC_POOL_ADDRESS);
-  const ArbitUsdExchangeRate = await getPoolexchangeRate(process.env.ARB_USDC_POOL_ADDRESS);
+  const etherUsdExchangeRate = await getPoolexchangeRate(
+    process.env.ETHER_USDC_POOL_ADDRESS
+  );
+  const ArbitUsdExchangeRate = await getPoolexchangeRate(
+    process.env.ARB_USDC_POOL_ADDRESS
+  );
   const postionDataFromContract = await getPostionData(poolId);
   const currentBlockNumber = await getCurrentBlockNumber();
-  await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
+  analyzeDataPoint(postionDataFromContract);
+  // await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
   //const postionDataFromTheGraph = await queryTheGraph(poolId);
   //await checkForAlerts(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
   //await notifiy();
@@ -26,4 +37,3 @@ async function main() {
     loop();
   }, process.env.INTERVAL);
 })();
-
