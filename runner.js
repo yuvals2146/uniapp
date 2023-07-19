fbf49836e59f655326ab57c261c4c8ad74a0bdee
yuvals2146 <yuvals2146@gmail.com>
2023-07-19 19:14:51 +0300

    main

--- runner.js
+++ runner.js
@@ -16,14 +16,6 @@ const logger = require("./logger.js");
 const init = async () => {
   //await initNotifer();
   await logger.initLogger();
-  logger.debug("debug", "debug");
-  logger.info("init done");
-  logger.error("error!");
-};
-
-const init = async () => {
-  //await initNotifer();
-  await logger.initLogger();
   logger.debug('debug','debug');
   logger.info('init done');
   logger.error('error!');
