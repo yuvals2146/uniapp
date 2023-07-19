29dfeec68c6f48cb56a7d2911be27affe14c6554
azrield <azrield@gmail.com>
2023-07-19 21:23:53 +0300

    add position ref to positionData

--- notifer.js
+++ notifer.js
@@ -1,7 +1,11 @@
-const { Client } = require('whatsapp-web.js');
-const push = require( 'pushover-notifications' )
-const qrcode = require('qrcode-terminal');
-const {poolAboveTreshold, poolBelowTrashhold, USDCIsBelowTreshold} = require('./alertsTemplates.js');
+const { Client } = require("whatsapp-web.js");
+const push = require("pushover-notifications");
+const qrcode = require("qrcode-terminal");
+const {
+  poolAboveTreshold,
+  poolBelowTrashhold,
+  USDCIsBelowTreshold,
+} = require("./alertsTemplates.js");
 require("dotenv").config();
 const logger = require("./logger.js");
 
@@ -9,82 +13,77 @@ const client = new Client();
 let pushover;
 
 const initNotifer = async () => {
+  if (
+    process.env.PUSHOVER_USER === undefined ||
+    process.env.PUSHOVER_TOKEN === undefined
+  ) {
+    logger.error("Pushover user or token not defined");
+    process.exit(1);
+  }
 
-    if( process.env.PUSHOVER_USER === undefined || process.env.PUSHOVER_TOKEN === undefined) {
-        logger.error('Pushover user or token not defined');
-        process.exit(1);
-    }
+  try {
+    pushover = new push({
+      user: process.env.PUSHOVER_USER,
+      token: process.env.PUSHOVER_TOKEN,
+    });
 
-    try {
-    pushover = new push( {
-        user: process.env.PUSHOVER_USER,
-        token: process.env.PUSHOVER_TOKEN,
-      })
-    
-    client.on('qr', qr => {
-        qrcode.generate(qr, {small: true});
+    client.on("qr", (qr) => {
+      qrcode.generate(qr, { small: true });
     });
-    
-    client.on('ready', () => {
-        console.log('Client is ready!');
+
+    client.on("ready", () => {
+      console.log("Client is ready!");
     });
-    
+
     client.initialize();
-} catch (err) {
-    console.log(err)
-}
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 initNotifer();
 
+const pushoverNotify = async (text, title) => {
+  var msg = {
+    message: text, // required
+    title: title,
+    sound: "magic",
+    device: "devicename",
+    priority: 1,
+  };
 
-
-
-const pushoverNotify = async (text, title) => {       
-    var msg = {
-      message: text,	// required
-      title: title,
-      sound: 'magic',
-      device: 'devicename',
-      priority: 1
+  pushover.send(msg, function (err, result) {
+    if (err) {
+      console.log(err);
+      throw err;
     }
-     
-    pushover.send( msg, function( err, result ) {
-      if ( err ) {
-        console.log(err)
-        throw err
-      }
-      console.log( result )
-    })
-  }
-
-const whatsappNotify = async (text,title) => {
-    msg = title + "\n" + text;
-    client.sendMessage(process.env.WHATSAPP_ALERT_GROUP, msg);
-}
-
-const botAddNewPosition = async (position) => {
-    whatsappNotify('*adding position*', 'what is the position number?');
+    console.log(result);
+  });
 };
 
+const whatsappNotify = async (text, title) => {
+  msg = title + "\n" + text;
+  client.sendMessage(process.env.WHATSAPP_ALERT_GROUP, msg);
+};
 
-client.on('message', message => {
-	console.log(message);
-    if(message.body.includes('add new position')) {
-        botAddNewPosition(message);        
-    }
+const botAddNewPosition = async (position) => {
+  whatsappNotify("*adding position*", "what is the position number?");
+};
+
+client.on("message", (message) => {
+  console.log(message);
+  if (message.body.includes("add new position")) {
+    botAddNewPosition(message);
+  }
 });
- 
 
-
-const notifiy = async (text,title) => {
-  //const msg = "this is anautmated message: \n" + "please check pool, pool id: 689765 \n" + "reach upper trashold \n" + "reach lower trashold \n" + "https://app.uniswap.org/#/tokens/ethereum/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
-  pushoverNotify(text,title);
-  whatsappNotify(text,title);
-}
+const notifiy = async (text, title) => {
+  pushoverNotify(text, title);
+  whatsappNotify(text, title);
+};
 
 module.exports = {
-    notifiy,
-    initNotifer,
-    pushoverNotify
-}
+  notifiy,
+  initNotifer,
+  pushoverNotify,
+};
--- runner.js
+++ runner.js
@@ -4,25 +4,27 @@ const {
   getCurrentBlockNumber,
 } = require("./getPostionData.js");
 const {
-  savePositionDataRedis,
+  saveOrValidateInitPositionInfo,
   savePositionDataSQL,
 } = require("./savePositionData.js");
 const { queryTheGraph } = require("./queryTheGraph.js");
 const { notifiy, initNotifer, pushoverNotify } = require("./notifer.js");
 const { checkForAlerts } = require("./alerts.js");
-const { analyzeDataPoint } = require("./engine/core_module.js");
+const { analyzeDataPoint } = require("./engine/analyzer.js");
 const logger = require("./logger.js");
+
+// validate_positions_db
 
 const init = async () => {
   //await initNotifer();
   await logger.initLogger();
-  logger.debug("debug", "debug");
-  logger.info("init done");
-  logger.error("error!");
+  saveOrValidateInitPositionInfo(parseInt(process.env.POSITION_ID));
+  // validate_positions_db();
+  logger.info("init", "done");
 };
+init();
 
-async function main() {
-  await init();
+async function data_routine() {
   const positionId = process.env.POSITION_ID;
   const etherUsdExchangeRate = await getPoolexchangeRate(
     process.env.ETHER_USDC_POOL_ADDRESS
@@ -34,15 +36,24 @@ async function main() {
   const postionDataFromContract = await getPostionData(positionId);
   const currentBlockNumber = await getCurrentBlockNumber();
   analyzeDataPoint(postionDataFromContract);
-  // await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
+  await savePositionDataSQL(
+    postionDataFromContract,
+    etherUsdExchangeRate,
+    ArbitUsdExchangeRate,
+    parseInt(positionId),
+    currentBlockNumber
+  );
   //const postionDataFromTheGraph = await queryTheGraph(poolId);
   //await checkForAlerts(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
-  await notifiy(`price1: ${etherUsdExchangeRate} \n price2: ${ArbitUsdExchangeRate} \n poolId: ${positionId}`, 'a GOOD message');
+  // await notifiy(
+  //   `price1: ${etherUsdExchangeRate} \n price2: ${ArbitUsdExchangeRate} \n poolId: ${positionId}`,
+  //   "a GOOD message"
+  // );
 }
 
 (function loop() {
   setTimeout(() => {
-    main();
+    data_routine();
 
     loop();
   }, process.env.INTERVAL);
