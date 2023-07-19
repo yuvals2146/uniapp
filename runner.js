760497d4cd3f1b44b98638ec7e3dcdc488db4bc6
yuvals2146 <yuvals2146@gmail.com>
2023-07-19 21:35:20 +0300

    fix

--- runner.js
+++ runner.js
@@ -4,25 +4,34 @@ const {
   getCurrentBlockNumber,
 } = require("./getPostionData.js");
 const {
-  savePositionDataRedis,
+  saveOrValidateInitPositionInfo,
   savePositionDataSQL,
 } = require("./savePositionData.js");
 const { queryTheGraph } = require("./queryTheGraph.js");
-const { notifiy, initNotifer, pushoverNotify } = require("./notifer.js");
+const {
+  notifiy,
+  initNotifer,
+  pushoverNotify,
+  isNotifierReady,
+} = require("./notifer.js");
 const { checkForAlerts } = require("./alerts.js");
-const { analyzeDataPoint } = require("./engine/core_module.js");
+const { analyzeDataPoint } = require("./engine/analyzer.js");
 const logger = require("./logger.js");
+
+// validate_positions_db
 
 const init = async () => {
   //await initNotifer();
   await logger.initLogger();
-  logger.debug('debug','debug');
-  logger.info('init done');
-  logger.error('error!');
-}
 
-async function main() {
-  await init();
+  saveOrValidateInitPositionInfo(parseInt(process.env.POSITION_ID));
+  // validate_positions_db();
+  logger.info("init", "done");
+};
+
+init();
+
+async function data_routine() {
   const positionId = process.env.POSITION_ID;
   const etherUsdExchangeRate = await getPoolexchangeRate(
     process.env.ETHER_USDC_POOL_ADDRESS
@@ -34,15 +43,24 @@ async function main() {
   const postionDataFromContract = await getPostionData(positionId);
   const currentBlockNumber = await getCurrentBlockNumber();
   analyzeDataPoint(postionDataFromContract);
-  // await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
+  await savePositionDataSQL(
+    postionDataFromContract,
+    etherUsdExchangeRate,
+    ArbitUsdExchangeRate,
+    parseInt(positionId),
+    currentBlockNumber
+  );
   //const postionDataFromTheGraph = await queryTheGraph(poolId);
   //await checkForAlerts(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
-  await notifiy(`price1: ${etherUsdExchangeRate} \n price2: ${ArbitUsdExchangeRate} \n poolId: ${positionId}`, 'a GOOD message');
+  // await notifiy(
+  //   `price1: ${etherUsdExchangeRate} \n price2: ${ArbitUsdExchangeRate} \n poolId: ${positionId}`,
+  //   "a GOOD message"
+  // );
 }
 
 (function loop() {
   setTimeout(() => {
-    main();
+    if (isNotifierReady()) data_routine();
 
     loop();
   }, process.env.INTERVAL);
