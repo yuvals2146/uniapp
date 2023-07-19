8888c8c2c88dbf741e598df6efef637b3aa88682
yuvals2146 <yuvals2146@gmail.com>
2023-07-19 19:02:38 +0300

    add shit

--- runner.js
+++ runner.js
@@ -8,12 +8,11 @@ const {
   savePositionDataSQL,
 } = require("./savePositionData.js");
 const { queryTheGraph } = require("./queryTheGraph.js");
-// const { notifiy, initNotifer } = require("./notifer.js");
+const { notifiy, initNotifer, pushoverNotify } = require("./notifer.js");
 const { checkForAlerts } = require("./alerts.js");
 const { analyzeDataPoint } = require("./engine/core_module.js");
 
 async function main() {
-  // initNotifer();
   const poolId = 689765;
   const etherUsdExchangeRate = await getPoolexchangeRate(
     process.env.ETHER_USDC_POOL_ADDRESS
@@ -21,13 +20,14 @@ async function main() {
   const ArbitUsdExchangeRate = await getPoolexchangeRate(
     process.env.ARB_USDC_POOL_ADDRESS
   );
-  const postionDataFromContract = await getPostionData(poolId);
-  const currentBlockNumber = await getCurrentBlockNumber();
-  analyzeDataPoint(postionDataFromContract);
+  
+  //const postionDataFromContract = await getPostionData(poolId);
+  //const currentBlockNumber = await getCurrentBlockNumber();
+  //analyzeDataPoint(postionDataFromContract);
   // await savePositionDataSQL(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId,currentBlockNumber);
   //const postionDataFromTheGraph = await queryTheGraph(poolId);
   //await checkForAlerts(postionDataFromContract,etherUsdExchangeRate,ArbitUsdExchangeRate, poolId);
-  //await notifiy();
+  await notifiy(`price1: ${etherUsdExchangeRate} \n price2: ${ArbitUsdExchangeRate} \n poolId: ${poolId}`, 'a GOOD message');
 }
 
 (function loop() {
