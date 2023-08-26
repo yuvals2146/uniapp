b75b4b3bd82ce6f0172ac7fc864e2b69e3b96ae1
yuvals2146 <yuvals2146@gmail.com>
2023-08-26 08:54:06 +0300

    add first test

--- test/iamtest.test.js
+++ test/iamtest.test.js
@@ -3,3 +3,4 @@ describe("addNewPosition", () => {
     expect(1).toEqual(1);
   });
 });
+
---
+++ test/theGraph/getTxHashFromPositionId.test.js
@@ -0,0 +1,12 @@
+const chains = require("../../src/config/chains");
+const queryTheGraph = require("../../src/utils/queryTheGraph");
+
+const validPosition = {
+  id: 482139,
+  chain: 1,
+};
+describe("TheGraph - get Tx Hash From Position Id", () => {
+  test("should retreive valid tx hash for position id", async () => {
+    expect(1).toEqual(1);
+  });
+});
