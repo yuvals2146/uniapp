312a446e5dd07023d656dab55a3c0844c4f3d626
azrield <azrield@gmail.com>
2023-12-14 00:01:12 +0200

    accumulating liquidity tick by tick

--- src/blockchain/TempLiquidityTool.js
+++ src/blockchain/TempLiquidityTool.js
@@ -10,29 +10,31 @@ const etherProvider = new ethers.providers.JsonRpcProvider(
   process.env.ETHER_RPC_URL
 );
 
+const WETHOnMainnet = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const USDCOnMainnet = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 const Q96 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(96));
 
 const getPoolTest = async (chain) => {
-  const WETH_TOKEN = new Token(
+  const TOKEN0 = new Token(
     SupportedChainId.MAINNET,
-    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    18,
-    "WETH",
-    "Wrapped Ether"
-  );
-
-  const USDC_TOKEN = new Token(
-    SupportedChainId.MAINNET,
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    USDCOnMainnet,
     6,
     "USDC",
     "USD//C"
   );
 
+  const TOKEN1 = new Token(
+    SupportedChainId.MAINNET,
+    WETHOnMainnet,
+    18,
+    "WETH",
+    "Wrapped Ether"
+  );
+
   const CurrentConfig = {
     tokens: {
-      token0: USDC_TOKEN,
-      token1: WETH_TOKEN,
+      token0: TOKEN0,
+      token1: TOKEN1,
       poolFee: FeeAmount.LOW,
     },
   };
@@ -50,7 +52,7 @@ const getPoolTest = async (chain) => {
     etherProvider
   );
   const slot0 = await poolContract.slot0();
-
+  const currLiquidity = await poolContract.liquidity();
   const tickSpacing = await poolContract.tickSpacing();
   currTick = slot0[1];
 
@@ -62,61 +64,77 @@ const getPoolTest = async (chain) => {
     "Tick min:",
     TickMath.MIN_TICK,
     "Tick max:",
-    TickMath.MAX_TICK
+    TickMath.MAX_TICK,
+    "Current liquidity:",
+    currLiquidity.toString()
   );
 
-  currTickRounded = Math.floor(currTick / tickSpacing) * tickSpacing;
-  currTickRounded = currTickRounded - tickSpacing;
-  currTick = currTick - tickSpacing;
+  const currTickRounded = Math.floor(currTick / tickSpacing) * tickSpacing;
+  // currTickRounded = currTickRounded - tickSpacing;
+  // currTick = currTick - tickSpacing;
+  MIN_TICK = -10;
+  MAX_TICK = 10;
+  liquidityAtTick = [];
+  liquidityAtTick[0] = parseInt(currLiquidity);
+  var tempTickRounded = currTickRounded;
+  for (let i = 1; i < MAX_TICK; i++) {
+    tempTickRounded += tickSpacing;
+    tick = await poolContract.ticks(tempTickRounded);
+    liquidityNetAtTick = parseInt(tick.liquidityNet);
+    liquidityAtTick[i] = liquidityAtTick[i - 1] + liquidityNetAtTick;
+  }
+  tempTickRounded = currTickRounded;
+  for (let i = -1; i >= -MIN_TICK; i--) {
+    tempTickRounded -= tickSpacing;
+    tick = await poolContract.ticks(tempTickRounded);
+    liquidityNetAtTick = parseInt(tick.liquidityNet);
+    liquidityAtTick[i] = liquidityAtTick[i + 1] + liquidityNetAtTick;
+  }
 
-  for (let i = 0; i < 3; i++) {
-    tick = await poolContract.ticks(currTickRounded);
-    sqrtRatioAtTickX96 = TickMath.getSqrtRatioAtTick(currTick, tickSpacing);
+  for (let i = MIN_TICK; i < MAX_TICK; i++) {
+    tempTickRounded = currTickRounded + i * tickSpacing;
+    sqrtRatioAtTickX96 = TickMath.getSqrtRatioAtTick(
+      tempTickRounded,
+      tickSpacing
+    );
     const sqrtRatioX96BN = slot0[0];
-    const sqrtRatioX96BI =
-      currTick === slot0[1]
-        ? JSBI.BigInt(sqrtRatioX96BN.toString())
-        : sqrtRatioAtTickX96;
+    const sqrtRatioX96BI = sqrtRatioAtTickX96;
     const buyOneOfToken0 =
       (sqrtRatioX96BI / Q96) ** 2 /
       (
-        10 ** CurrentConfig.tokens.token1.decimals /
-        10 ** CurrentConfig.tokens.token0.decimals
+        10 **
+        (CurrentConfig.tokens.token1.decimals -
+          CurrentConfig.tokens.token0.decimals)
       ).toFixed(CurrentConfig.tokens.token1.decimals);
     const buyOneOfToken1 = (1 / buyOneOfToken0).toFixed(
       CurrentConfig.tokens.token0.decimals
     );
-    const buyOneOfToken0Wei = Math.floor(
-      buyOneOfToken0 * 10 ** CurrentConfig.tokens.token1.decimals
-    ).toLocaleString("fullwide", { useGrouping: false });
-    const buyOneOfToken1Wei = Math.floor(
-      buyOneOfToken1 * 10 ** CurrentConfig.tokens.token0.decimals
-    ).toLocaleString("fullwide", { useGrouping: false });
 
     tokenAmountsAtTickRange = await getTokenAmounts(
-      tick.liquidityNet,
+      liquidityAtTick[i],
       sqrtRatioX96BI,
-      currTickRounded,
-      currTickRounded + tickSpacing,
-      (i - 1) * tickSpacing,
+      tempTickRounded,
+      tempTickRounded + tickSpacing,
       CurrentConfig.tokens.token0.decimals,
       CurrentConfig.tokens.token1.decimals
     );
 
     console.log(
-      "\nIteration:",
-      i,
-      "Current tick:",
-      currTick,
-      "Current tick rounded:",
-      currTickRounded,
-      "sqrtRatioX96 at tick:",
-      sqrtRatioAtTickX96.toString(),
-      "sqrtRatioX96 at slot0:",
-      sqrtRatioX96BI.toString(),
-      "\nPrice of Token0 in Token1 terms",
+      "Tick",
+      currTickRounded + i * tickSpacing,
+      ":",
+      liquidityAtTick[i],
+      "\nPrice of ",
+      TOKEN0.symbol,
+      " in ",
+      TOKEN1.symbol,
+      " terms:",
       buyOneOfToken0,
-      "Price of Token1 in Token0 terms",
+      "Price of ",
+      TOKEN1.symbol,
+      " in ",
+      TOKEN0.symbol,
+      " terms:",
       buyOneOfToken1,
       "\nLiquidity:",
       BigInt(tick.liquidityGross).toString(),
@@ -125,32 +143,64 @@ const getPoolTest = async (chain) => {
       "Liquidity in Token1:",
       tokenAmountsAtTickRange[1]
     );
-    currTick += tickSpacing;
-    currTickRounded += tickSpacing;
   }
 };
+// console.log(
+//   "Tick rounded: ",
+//   currTickRounded,
+//   ", ",
+//   USDC_TOKEN.symbol,
+//   " liquidity: ",
+//   liquidityToken0,
+//   ", ",
+//   WETH_TOKEN.symbol,
+//   " liquidity: ",
+//   liquidityToken1
+// );
+
+// const buyOneOfToken0Wei = Math.floor(
+//   buyOneOfToken0 * 10 ** CurrentConfig.tokens.token1.decimals
+// ).toLocaleString("fullwide", { useGrouping: false });
+// const buyOneOfToken1Wei = Math.floor(
+//   buyOneOfToken1 * 10 ** CurrentConfig.tokens.token0.decimals
+// ).toLocaleString("fullwide", { useGrouping: false });
+
+//     console.log(
+//       "\nIteration:",
+//       i,
+//       "Current tick:",
+//       currTick,
+//       "Current tick rounded:",
+//       currTickRounded,
+//       "sqrtRatioX96 at tick:",
+//       sqrtRatioAtTickX96.toString(),
+//       "sqrtRatioX96 at slot0:",
+//       sqrtRatioX96BI.toString(),
+
+//     );
+//     currTick += tickSpacing;
+//     currTickRounded += tickSpacing;
+//   }
+// };
 
 const getTokenAmounts = async (
   liquidity,
   sqrtPriceX96,
   tickLow,
   tickHigh,
-  delta,
   token0Decimal,
   token1Decimal
 ) => {
   let sqrtRatioA = Math.sqrt(1.0001 ** tickLow).toFixed(18);
   let sqrtRatioB = Math.sqrt(1.0001 ** tickHigh).toFixed(18);
-  let currentTick = TickMath.getTickAtSqrtRatio(sqrtPriceX96) + delta;
+  let currentTick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
   console.log(
     "\n\ngetTokenAmounts:",
     currentTick,
     tickLow,
     tickHigh,
     "liq at tick",
-    TickMath.getTickAtSqrtRatio(sqrtPriceX96),
-    "delta:",
-    delta
+    liquidity
   );
   let sqrtPrice = sqrtPriceX96 / Q96;
   let amount0wei = 0;
